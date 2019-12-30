e283125b90d9252315e4d24ec9f5ce68cd0e9ee5
Benjamin Cohn <cohnbj13@gmail.com>
2019-12-30 23:38:35 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -169,12 +169,11 @@ def play(board)
   until over?(board)
     turn(board)
 
-    if won?(board)
-      if winner(board)=="X"
-        puts "Congratulations X!"
-      elsif winner(board)=="O"
-        puts "Congratulations O!"
-      end
+    
+    if winner(board)=="X"
+      puts "Congratulations X!"
+    elsif winner(board)=="O"
+      puts "Congratulations O!"
     elsif draw?(board)
       puts "Cat's Game!"
     end
