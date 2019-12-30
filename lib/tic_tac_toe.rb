ceb54f18c8af542ba6accdca5ae92513a1fc8915
Benjamin Cohn <cohnbj13@gmail.com>
2019-12-30 23:29:36 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -169,3 +169,9 @@ def play(board)
   until over?(board)
     turn(board)
   end
+  if won?(board)
+    puts "Congratulations {winner(board)}"
+  elsif draw?(board)
+    puts "There has been a draw"
+  end
+end
